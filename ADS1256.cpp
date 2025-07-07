68e041caf4f8f4b26d58d15eb5b1dee99b7bba0a
freudenthal <john.freudenthal@gmail.com>
2025-07-07 13:06:18 -0700

    Added SPI pointer and connection settings. Begin and end transactions, and calculate the delays and clock rates.

--- ADS1256.cpp
+++ ADS1256.cpp
@@ -1,22 +1,25 @@
 /*
-        ADS1256.h - Arduino Library for communication with Texas Instrument ADS1256 ADC
-        Written by Adien Akhmad, August 2015
-		Modfified  Jan 2019 by Axel Sepulveda for ATMEGA328
+  ADS1256.h - Arduino Library for communication with Texas Instrument ADS1256 ADC
+  Written by Adien Akhmad, August 2015
+  Modfified  Jan 2019 by Axel Sepulveda for ATMEGA328
+  Modified July 2025 by John Freudenthal to work with alternative SPI buses and connection settings.
 */
 
 #include "ADS1256.h"
 #include "Arduino.h"
 #include "SPI.h"
 
-ADS1256::ADS1256(float clockspdMhz, float vref, uint8_t cspin, uint8_t readypin = 254, uint8_t resetpin = 254)
+ADS1256::ADS1256(SPIClass* spi, float clockspdMhz, float vref, uint8_t cspin, uint8_t readypin = 254, uint8_t resetpin = 254)
 {
-  ConnectionSettings = SPISettings(clockspdMhz * 1000000 / 4, MSBFIRST, SPI_MODE1);
+  SPIBus = spi;
+  uint32_t SPISpeed = 1000000; //or clockspdMhz * 1000000 / 4 //May also want 5MHz at most
+  ConnectionSettings = SPISettings(SPISpeed, MSBFIRST, SPI_MODE1);
   ReadyPin = readypin;
   ResetPin = resetpin;
   CSPin = cspin;
   if (ReadyPin != 254)
   {
-    UseReady = true; 
+    UseReady = true;
   }
   else
   {
@@ -34,75 +37,89 @@ ADS1256::ADS1256(float clockspdMhz, float vref, uint8_t cspin, uint8_t readypin 
   _VREF = vref;
   // Default conversion factor
   _conversionFactor = 1.0;
-  // Start SPI on a quarter of ADC clock speed
-  SPI.begin();
-  SPI.beginTransaction();
+  DelayT6 = (uint16_t)((1.0/(float)(clockspdMhz)) * 50.0) + 1;
+  DelayT11 = (uint16_t)((1.0/(float)(clockspdMhz)) * 4.0) + 1;
 }
 
-void ADS1256::writeRegister(unsigned char reg, unsigned char wdata) {
+void ADS1256::writeRegister(unsigned char reg, unsigned char wdata)
+{
+  SPIBus->beginTransaction(ConnectionSettings);
   CSON();
-  SPI.transfer(ADS1256_CMD_WREG | reg); // opcode1 Write registers starting from reg
-  SPI.transfer(0);  // opcode2 Write 1+0 registers
-  SPI.transfer(wdata);  // write wdata
-  delayMicroseconds(1);              
+  SPIBus->transfer(ADS1256_CMD_WREG | reg); // opcode1 Write registers starting from reg
+  SPIBus->transfer(0); // opcode2 Write 1+0 registers
+  SPIBus->transfer(wdata); // write wdata
+  delayMicroseconds(DelayT11);
+  SPIBus->endTransaction();
   CSOFF();
 }
 
-unsigned char ADS1256::readRegister(unsigned char reg) {
+unsigned char ADS1256::readRegister(unsigned char reg)
+{
   unsigned char readValue;
+  SPIBus->beginTransaction(ConnectionSettings);
   CSON();
-  SPI.transfer(ADS1256_CMD_RREG | reg); // opcode1 read registers starting from reg
-  SPI.transfer(0);                  // opcode2 read 1+0 registers
-  delayMicroseconds(7);              //  t6 delay (4*tCLKIN 50*0.13 = 6.5 us)    
-  readValue = SPI.transfer(0);          // read registers
-  delayMicroseconds(1);              //  t11 delay (4*tCLKIN 4*0.13 = 0.52 us)    
+  SPIBus->transfer(ADS1256_CMD_RREG | reg); // opcode1 read registers starting from reg
+  SPIBus->transfer(0); // opcode2 read 1+0 registers
+  delayMicroseconds(DelayT6); // t6 delay (4*tCLKIN 50*0.13 = 6.5 us)
+  readValue = SPIBus->transfer(0); // read registers
+  delayMicroseconds(DelayT11); // t11 delay (4*tCLKIN 4*0.13 = 0.52 us)
   CSOFF();
+  SPIBus->endTransaction();
   return readValue;
-  
 }
 
-void ADS1256::sendCommand(unsigned char reg) {
+void ADS1256::sendCommand(unsigned char reg)
+{
+  SPIBus->beginTransaction(ConnectionSettings);
   CSON();
   waitDRDY();
-  SPI.transfer(reg);
-  delayMicroseconds(1);              //  t11 delay (4*tCLKIN 4*0.13 = 0.52 us)    
+  SPIBus->transfer(reg);
+  delayMicroseconds(DelayT11); // t11 delay (4*tCLKIN 4*0.13 = 0.52 us)
   CSOFF();
+  SPIBus->endTransaction();
 }
 
-void ADS1256::setConversionFactor(float val) { _conversionFactor = val; }
+void ADS1256::setConversionFactor(float val)
+{
+  _conversionFactor = val;
+}
 
 void ADS1256::readTest()
 {
   unsigned char _highByte, _midByte, _lowByte;
+  SPIBus->beginTransaction(ConnectionSettings);
   CSON();
-  SPI.transfer(ADS1256_CMD_RDATA);
-  delayMicroseconds(7);              //  t6 delay (4*tCLKIN 50*0.13 = 6.5 us)    
-
-  _highByte = SPI.transfer(ADS1256_CMD_WAKEUP);
-  _midByte = SPI.transfer(ADS1256_CMD_WAKEUP);
-  _lowByte = SPI.transfer(ADS1256_CMD_WAKEUP);
-
+  SPIBus->transfer(ADS1256_CMD_RDATA);
+  delayMicroseconds(DelayT6); // t6 delay (4*tCLKIN 50*0.13 = 6.5 us)
+  _highByte = SPIBus->transfer(ADS1256_CMD_WAKEUP);
+  _midByte = SPIBus->transfer(ADS1256_CMD_WAKEUP);
+  _lowByte = SPIBus->transfer(ADS1256_CMD_WAKEUP);
   CSOFF();
+  SPIBus->endTransaction();
 }
 
 float ADS1256::readCurrentChannel()
 {
+  SPIBus->beginTransaction(ConnectionSettings);
   CSON();
-  SPI.transfer(ADS1256_CMD_RDATA);
-  delayMicroseconds(7);              //  t6 delay (4*tCLKIN 50*0.13 = 6.5 us)              
+  SPIBus->transfer(ADS1256_CMD_RDATA);
+  delayMicroseconds(DelayT6); // t6 delay (4*tCLKIN 50*0.13 = 6.5 us)
   float adsCode = read_float32();
   CSOFF();
+  SPIBus->endTransaction();
   return ((adsCode / 0x7FFFFF) * ((2 * _VREF) / (float)_pga)) * _conversionFactor;
 }
 
 // Reads raw ADC data, as 32bit int
 long ADS1256::readCurrentChannelRaw()
 {
+  SPIBus->beginTransaction(ConnectionSettings);
   CSON();
-  SPI.transfer(ADS1256_CMD_RDATA);
-  delayMicroseconds(7);              //  t6 delay (4*tCLKIN 50*0.13 = 6.5 us)       
+  SPIBus->transfer(ADS1256_CMD_RDATA);
+  delayMicroseconds(DelayT6); // t6 delay (4*tCLKIN 50*0.13 = 6.5 us)
   long adsCode = read_int32();
   CSOFF();
+  SPIBus->endTransaction();
   return adsCode;
 }
 
@@ -111,11 +128,9 @@ unsigned long ADS1256::read_uint24()
 {
   unsigned char _highByte, _midByte, _lowByte;
   unsigned long value;
-
   _highByte = SPI.transfer(0);
   _midByte  = SPI.transfer(0);
   _lowByte  = SPI.transfer(0);
-
   // Combine all 3-bytes to 24-bit data using byte shifting.
   value = ((long)_highByte << 16) + ((long)_midByte << 8) + ((long)_lowByte);
   return value;
@@ -126,11 +141,10 @@ unsigned long ADS1256::read_uint24()
 long ADS1256::read_int32()
 {
   long value = read_uint24();
-
-  if (value & 0x00800000) { // if the 24 bit value is negative reflect it to 32bit
+  if (value & 0x00800000) // if the 24 bit value is negative reflect it to 32bit
+  {
     value |= 0xff000000;
   }
-
   return value;
 }
 
@@ -142,22 +156,20 @@ float ADS1256::read_float32()
   return (float)value;
 }
 
-// Channel switching for single ended mode. Negative input channel are
-// automatically set to AINCOM
+// Channel switching for single ended mode. Negative input channel are automatically set to AINCOM
 void ADS1256::setChannel(byte channel)
 {
   setChannel(channel, -1);
 }
 
-// Channel Switching for differential mode. Use -1 to set input channel to
-// AINCOM
+// Channel Switching for differential mode. Use -1 to set input channel to AINCOM
 void ADS1256::setChannel(byte AIN_P, byte AIN_N)
 {
   unsigned char MUX_CHANNEL;
   unsigned char MUXP;
   unsigned char MUXN;
-
-  switch (AIN_P) {
+  switch (AIN_P)
+  {
     case 0:
       MUXP = ADS1256_MUXP_AIN0;
       break;
@@ -185,8 +197,8 @@ void ADS1256::setChannel(byte AIN_P, byte AIN_N)
     default:
       MUXP = ADS1256_MUXP_AINCOM;
   }
-
-  switch (AIN_N) {
+  switch (AIN_N)
+  {
     case 0:
       MUXN = ADS1256_MUXN_AIN0;
       break;
@@ -214,9 +226,7 @@ void ADS1256::setChannel(byte AIN_P, byte AIN_N)
     default:
       MUXN = ADS1256_MUXN_AINCOM;
   }
-
   MUX_CHANNEL = MUXP | MUXN;
-
   CSON();
   writeRegister(ADS1256_RADD_MUX, MUX_CHANNEL);
   sendCommand(ADS1256_CMD_SYNC);
@@ -236,27 +246,26 @@ void ADS1256::begin(unsigned char drate, unsigned char gain, bool buffenable)
   if (UseReset)
   {
     // set RESETPIN as output
-    pinMode(ResetPin, OUTPUT );
+    pinMode(ResetPin, OUTPUT);
     // pull RESETPIN high
     pinMode(pinRST, HIGH);
   }
   pinMode(CSPin, OUTPUT);
   pinMode(CSPin, HIGH);
-
   _pga = 1 << gain;
-  sendCommand(ADS1256_CMD_SDATAC);  // send out ADS1256_CMD_SDATAC command to stop continous reading mode.
-  writeRegister(ADS1256_RADD_DRATE, drate);  // write data rate register   
+  sendCommand(ADS1256_CMD_SDATAC); // send out ADS1256_CMD_SDATAC command to stop continous reading mode.
+  writeRegister(ADS1256_RADD_DRATE, drate); // write data rate register
   uint8_t bytemask = B00000111;
   uint8_t adcon = readRegister(ADS1256_RADD_ADCON);
   uint8_t byte2send = (adcon & ~bytemask) | gain;
   writeRegister(ADS1256_RADD_ADCON, byte2send);
   if (buffenable)
   {  
-    uint8_t status = readRegister(ADS1256_RADD_STATUS);   
+    uint8_t status = readRegister(ADS1256_RADD_STATUS);
     bitSet(status, 1); 
     writeRegister(ADS1256_RADD_STATUS, status);
   }
-  sendCommand(ADS1256_CMD_SELFCAL);  // perform self calibration
+  sendCommand(ADS1256_CMD_SELFCAL); // perform self calibration
   if (UseReady)
   {
     waitDRDY();
@@ -268,7 +277,7 @@ Reads and returns STATUS register
 */ 
 uint8_t ADS1256::getStatus()
 {
-  sendCommand(ADS1256_CMD_SDATAC);  // send out ADS1256_CMD_SDATAC command to stop continous reading mode.
+  sendCommand(ADS1256_CMD_SDATAC); // send out ADS1256_CMD_SDATAC command to stop continous reading mode.
   return readRegister(ADS1256_RADD_STATUS); 
 }
 
@@ -289,7 +298,25 @@ void ADS1256::CSOFF()
 
 void ADS1256::waitDRDY()
 {
-  while (digitalRead(pinDRDY));
+  bool KeepWaiting = true;
+  uint32_t WaitStart = micros();
+  bool PinState = true;
+  while (KeepWaiting)
+  {
+      PinState = digitalRead(pinDRDY);
+      if (!PinState)
+      {
+        KeepWaiting = false;
+      }
+      else
+      {
+        if ( (micros() - WaitStart) > 1000000)
+        {
+          Serial.println("ADS1256 error on waitDRDY.");
+          KeepWaiting = false;
+        }
+      }
+  }
 }
 
 boolean ADS1256::isDRDY()
--- ADS1256.h
+++ ADS1256.h
@@ -1,41 +1,12 @@
 /*
-        ADS1256.h - Arduino Library for communication with Texas Instrument ADS1256 ADC
-        Written by Adien Akhmad, August 2015
-	Modifified  Jan 2019 by Axel Sepulveda for ATMEGA328
+  ADS1256.h - Arduino Library for communication with Texas Instrument ADS1256 ADC
+  Written by Adien Akhmad, August 2015
+  Modifified  Jan 2019 by Axel Sepulveda for ATMEGA328
+  Modified July 2025 by John Freudenthal to work with alternative SPI buses and connection settings.
 */
 
 #ifndef ADS1256_h
 #define ADS1256_h
-
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-
-    #define pinDRDY 9
-    #define pinRST  8
-    #define pinCS   10 
-    
-#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-
-    #define pinDRDY 49
-    #define pinRST  48
-    #define pinCS   53 
-	
- // Contributions are welcome   
-#elif   defined(ARDUINO_ARCH_ESP8266)
-//https://esp8266-shop.com/esp8266-guide/esp8266-nodemcu-pinout/
-    #define pinDRDY D0
-    #define pinRST  D1
-    #define pinCS   D8 // D8 Hw Cs in esp8266
-
-#elif   defined(ARDUINO_ARCH_ESP32)
-	// Contributions are welcome
-    //https://circuits4you.com/wp-content/uploads/2018/12/ESP32-Pinout.jpg
-    #define pinDRDY 17
-    #define pinRST  16
-    #define pinCS   5 //  
-#else 
-	// Contributions are welcome
-	#warning  "Oops! Pins for your board are not defined: pinDRDY, pinRST, pinCS"
-#endif
 
 // ADS1256 Register address
 #define ADS1256_RADD_STATUS 0x00
@@ -49,7 +20,6 @@
 #define ADS1256_RADD_FSC0 0x08
 #define ADS1256_RADD_FSC1 0x09
 #define ADS1256_RADD_FSC2 0x0A
-
 // ADS1256 Command
 #define ADS1256_CMD_WAKEUP 0x00
 #define ADS1256_CMD_RDATA 0x01
@@ -65,8 +35,7 @@
 #define ADS1256_CMD_SYNC 0xFC
 #define ADS1256_CMD_STANDBY 0xFD
 #define ADS1256_CMD_RESET 0xFE
-
-// define multiplexer codes
+// define multiplexer positive codes
 #define ADS1256_MUXP_AIN0 0x00
 #define ADS1256_MUXP_AIN1 0x10
 #define ADS1256_MUXP_AIN2 0x20
@@ -76,7 +45,7 @@
 #define ADS1256_MUXP_AIN6 0x60
 #define ADS1256_MUXP_AIN7 0x70
 #define ADS1256_MUXP_AINCOM 0x80
-
+// define multiplexer negative codes
 #define ADS1256_MUXN_AIN0 0x00
 #define ADS1256_MUXN_AIN1 0x01
 #define ADS1256_MUXN_AIN2 0x02
@@ -86,7 +55,6 @@
 #define ADS1256_MUXN_AIN6 0x06
 #define ADS1256_MUXN_AIN7 0x07
 #define ADS1256_MUXN_AINCOM 0x08
-
 // define gain codes
 #define ADS1256_GAIN_1 0x00
 #define ADS1256_GAIN_2 0x01
@@ -95,14 +63,10 @@
 #define ADS1256_GAIN_16 0x04
 #define ADS1256_GAIN_32 0x05
 #define ADS1256_GAIN_64 0x06
-
 // define drate codes
 /*
-        NOTE : 	Data Rate vary depending on crystal frequency. Data rates
-   listed below assumes the crystal frequency is 7.68Mhz
-                for other frequency consult the datasheet.
+  NOTE : Data Rate vary depending on crystal frequency. Data rates listed below assumes the crystal frequency is 7.68Mhz for other frequency consult the datasheet.
 */
-
 #define ADS1256_DRATE_30000SPS 0xF0
 #define ADS1256_DRATE_15000SPS 0xE0
 #define ADS1256_DRATE_7500SPS 0xD0
@@ -123,9 +87,10 @@
 #include "Arduino.h"
 #include "SPI.h"
 
-class ADS1256 {
+class ADS1256
+{
     public:
-        ADS1256(float clockspdMhz, float vref, uint8_t cspin, uint8_t readypin, uint8_t resetpin);
+        ADS1256(SPIClass* spi, float clockspdMhz, float vref, uint8_t cspin, uint8_t readypin, uint8_t resetpin);
         void begin(unsigned char drate, unsigned char gain, bool bufferenable);
         void writeRegister(unsigned char reg, unsigned char wdata);
         unsigned char readRegister(unsigned char reg);
@@ -142,6 +107,7 @@ class ADS1256 {
         void readTest();
         SPISettings* GetSPISettings();
     private:
+        SPIClass* SPIBus;
         SPISettings ConnectionSettings;
         void CSON();
         void CSOFF();
@@ -156,6 +122,8 @@ class ADS1256 {
         uint8_t ResetPin;
         uint8_t ReadyPin;
         uint8_t CSPin;
+        uint16_t DelayT11;
+        uint16_t DelayT6;
 };
 
 #endif
